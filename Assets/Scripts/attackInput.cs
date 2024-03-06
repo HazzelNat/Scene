e1c2cbda2490a6a6f24acbd044c3ec43c372b463
HazzelNat <155705238+HazzelNat@users.noreply.github.com>
2024-03-06 12:23:40 +0700

    SceneChange

---
+++ Assets/Scripts/attackInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackInput : MonoBehaviour
+{
+    public Animator charAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charAnimator = GetComponent<Animator>();
+    }
+
+    void Attack()
+    {   
+        print("Kepencet");
+        charAnimator.SetTrigger ("goAttack");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+}
